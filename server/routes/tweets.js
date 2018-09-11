ee28eeea660bb4f1e5c14e3123a0e5913b226e13
Sebastian Mantilla <sebastian.mantilla@gmail.com>
2018-09-11 23:58:47 +0000

    We do need Express there.

--- server/routes/tweets.js
+++ server/routes/tweets.js
@@ -2,7 +2,7 @@
 
 const userHelper    = require('../lib/util/user-helper');
 
-// const express       = require('express');
+const express       = require('express');
 const tweetsRoutes  = express.Router();
 
 module.exports = function(DataHelpers) {
