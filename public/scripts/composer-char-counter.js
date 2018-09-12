28d8c135c36750e564d769ffe5fceb1ebea8d677
Sebastian Mantilla <sebastian.mantilla@gmail.com>
2018-09-12 02:29:21 +0000

    We now have a working counter.

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,4 +1,11 @@
 $(document).ready(function() {
-  // --- our code goes here ---
-  console.log("Ready to go!");
+  $('textarea').on('keydown', function(event) {
+    let charactersLeft = 140 - this.value.length;
+    $(this).parent().children('span').text(charactersLeft);
+    if (charactersLeft < 0) {
+      $('.counter').addClass('exceded');
+    } else {
+      $('.counter').removeClass('exceded');
+    }
+  })
 });
