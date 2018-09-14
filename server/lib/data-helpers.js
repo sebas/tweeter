a5d5b6d06332c155e43470ef5008ba37faab1ce1
Sebastian Mantilla <sebastian.mantilla@gmail.com>
2018-09-14 21:41:01 +0000

    Code comments.

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -5,11 +5,15 @@ module.exports = function makeDataHelpers(db) {
   return {
 
     // Saves a tweet to `db`
+    // We are using Mongo DB which supports collections an documments.
+    // Our collection is tweets and we store documents, tweets in it.
     saveTweet: function(newTweet, callback) {
       db.collection("tweets").insertOne(newTweet, callback(null, true));
     },
 
-    // Get all tweets in `db`, sorted by newest first
+    // Get all tweets in `db`
+    // We query our collection for all the documents "tweets" that is contains
+    // (there are no conditions in the find) and we want an array back.
     getTweets: function(callback) {
       db.collection("tweets").find().toArray(callback);
     }
